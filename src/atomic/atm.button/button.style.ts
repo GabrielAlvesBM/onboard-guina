b6fcc734bc482fd08bf48c6c3233bc56cfb43d43
Gabriel Alves <gabrielalves6002@gmail.com>
2025-02-19 10:48:21 -0300

    refactor: change borders to outline

--- src/atomic/atm.button/button.style.ts
+++ src/atomic/atm.button/button.style.ts
@@ -5,15 +5,15 @@ export const buttonVariants = tv({
   variants: {
     variant: {
       primary:
-        "bg-brand-primary-dark hover:bg-brand-primary-x-dark focus:border-2 focus:border-cta active:bg-brand-primary-2x-dark",
+        "bg-brand-primary-dark hover:bg-brand-primary-x-dark focus:outline-2 focus:outline-cta active:bg-brand-primary-2x-dark",
       primaryDestructive:
-        "bg-feedback-error-medium hover:bg-feedback-error-dark focus:border-2 focus:border-feedback-error-dark active:bg-feedback-error-x-dark",
+        "bg-feedback-error-medium hover:bg-feedback-error-dark focus:outline-2 focus:outline-feedback-error-dark active:bg-feedback-error-x-dark",
       secondary:
-        "border-2 text-brand-primary-dark bg-transparent hover:border-brand-primary-x-dark hover:text-brand-primary-x-dark focus:border-3 active:border-brand-primary-2x-dark active:text-brand-primary-2x-dark",
+        "outline-2 text-brand-primary-dark bg-transparent hover:outline-brand-primary-x-dark hover:text-brand-primary-x-dark focus:outline-3 active:outline-brand-primary-2x-dark active:text-brand-primary-2x-dark",
       secondaryDestructive:
-        "border-2 border-feedback-error-medium text-feedback-error-medium bg-transparent hover:text-feedback-error-dark hover:border-feedback-error-dark focus:border-3 active:text-feedback-error-x-dark active:border-feedback-error-x-dark",
-      cta: "bg-x-dark hover:bg-dark focus:border-2 focus:border-2x-dark active:bg-2x-dark",
-      link: "py-3xs text-brand-primary-dark hover:underline focus:border-2 focus:border-brand-primary-dark active:text-brand-accessory-magenta",
+        "outline-2 outline-feedback-error-medium text-feedback-error-medium bg-transparent hover:text-feedback-error-dark hover:outline-feedback-error-dark focus:outline-3 active:text-feedback-error-x-dark active:outline-feedback-error-x-dark",
+      cta: "bg-x-dark hover:bg-dark focus:outline-2 focus:outline-2x-dark active:bg-2x-dark",
+      link: "py-3xs text-brand-primary-dark hover:underline focus:outline-2 focus:outline-brand-primary-dark active:text-brand-accessory-magenta",
     },
     disabled: {
       true: "opacity-50 pointer-events-none",
