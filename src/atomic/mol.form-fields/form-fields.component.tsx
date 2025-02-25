f993d75e189ee9b2180f12b234bb9a84963b376f
Gabriel Alves <gabrielalves6002@gmail.com>
2025-02-25 17:27:44 -0300

    refactor: change the formField logic

--- src/atomic/mol.form-fields/form-fields.component.tsx
+++ src/atomic/mol.form-fields/form-fields.component.tsx
@@ -13,28 +13,46 @@ const FormFields: FC<FormFieldsProps> = ({ fields }) => {
   return (
     <div className="flex flex-col gap-xs">
       {fields.map((field) => {
-        const isPassword = field.type === "password";
-        const InputComponent = isPassword ? PasswordInput : TextInput;
-
-        return (
-          <InputComponent
-            key={field.name}
-            label={field.label}
-            {...(isPassword ? {} : { type: field.type || "text" })}
-            placeholder={field.placeholder}
-            status={errors[field.name] ? "error" : field.status}
-            infoIcon={field.infoIcon}
-            {...register(field.name)}
-            caption={
-              errors[field.name]
-                ? {
-                    text: errors[field.name]?.message as string,
-                    status: "error",
-                  }
-                : undefined
-            }
-          />
-        );
+        if (field.type === "password") {
+          return (
+            <PasswordInput
+              key={field.name}
+              label={field.label}
+              placeholder={field.placeholder}
+              status={errors[field.name] ? "error" : field.status}
+              infoIcon={field.infoIcon}
+              {...register(field.name)}
+              caption={
+                errors[field.name]
+                  ? {
+                      text: errors[field.name]?.message as string,
+                      status: "error",
+                    }
+                  : undefined
+              }
+            />
+          );
+        } else {
+          return (
+            <TextInput
+              key={field.name}
+              label={field.label}
+              type={field.type || "text"}
+              placeholder={field.placeholder}
+              status={errors[field.name] ? "error" : field.status}
+              infoIcon={field.infoIcon}
+              {...register(field.name)}
+              caption={
+                errors[field.name]
+                  ? {
+                      text: errors[field.name]?.message as string,
+                      status: "error",
+                    }
+                  : undefined
+              }
+            />
+          );
+        }
       })}
     </div>
   );
