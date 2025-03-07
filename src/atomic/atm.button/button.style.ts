dc0878179583b29dd534b3e8481937c0b5024aca
GabrielAlvesBM <gabrielalves6002@gmail.com>
2025-03-07 18:22:56 -0300

    fix: linkButton shadows

--- src/atomic/atm.button/button.style.ts
+++ src/atomic/atm.button/button.style.ts
@@ -13,7 +13,7 @@ export const buttonVariants = tv({
       secondaryDestructive:
         "outline-2 outline-feedback-error-medium text-feedback-error-medium bg-transparent hover:text-feedback-error-dark hover:outline-feedback-error-dark focus:outline-3 active:text-feedback-error-x-dark active:outline-feedback-error-x-dark",
       cta: "bg-x-dark hover:bg-dark focus:outline-2 focus:outline-2x-dark active:bg-2x-dark",
-      link: "py-3xs text-brand-primary-dark hover:underline focus:outline-2 focus:outline-brand-primary-dark active:text-brand-accessory-magenta",
+      link: "py-3xs text-brand-primary-dark hover:underline focus:outline-2 focus:outline-brand-primary-dark active:text-brand-accessory-magenta shadow-none",
     },
     disabled: {
       true: "opacity-50 pointer-events-none",
