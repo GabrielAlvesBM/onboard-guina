7cc5364e282c8047985db68fa164cc200458e6af
Gabriel Alves <gabrielalves6002@gmail.com>
2025-03-13 10:35:08 -0300

    feat: add loggoutMiddleware and authLink

--- src/apollo/client.ts
+++ src/apollo/client.ts
@@ -1,7 +1,65 @@
-import { ApolloClient, InMemoryCache } from "@apollo/client";
+import { ApolloClient, InMemoryCache, HttpLink, from } from "@apollo/client";
+import { setContext } from "@apollo/client/link/context";
+import { onError } from "@apollo/client/link/error";
+import { useAuthStore } from "@/app/stores/auth.store";
+import { useUserStore } from "@/app/stores/user.store";
+import { jwtDecode } from "jwt-decode";
+
+const httpLink = new HttpLink({ uri: import.meta.env.VITE_SERVER_URL });
+
+const isTokenExpired = (token: string | null): boolean => {
+  try {
+    if (!token) {
+      return false;
+    }
+
+    const decoded: { exp: number } = jwtDecode(token);
+
+    if (!decoded.exp) {
+      return true;
+    }
+
+    return decoded.exp * 1000 < Date.now();
+  } catch {
+    return true;
+  }
+};
+
+const logoutUser = () => {
+  localStorage.removeItem("auth-store");
+  localStorage.removeItem("user-store");
+  useAuthStore.getState().clearAuth();
+  useUserStore.getState().clearUser();
+
+  window.location.href = "/auth/login";
+};
+
+const authLink = setContext((_, { headers }) => {
+  const token = useAuthStore.getState().token;
+
+  return {
+    headers: {
+      ...headers,
+      Authorization: token,
+    },
+  };
+});
+
+const logoutMiddleware = onError(({ networkError, graphQLErrors }) => {
+  const token = useAuthStore.getState().token;
+
+  if (graphQLErrors && isTokenExpired(token)) {
+    console.warn("Token expirado. Deslogando usuário...");
+    logoutUser();
+  }
+
+  if (networkError) {
+    console.log(`[Network Error]: ${networkError}`);
+  }
+});
 
 const client = new ApolloClient({
-  uri: import.meta.env.VITE_SERVER_URL,
+  link: from([authLink, logoutMiddleware, httpLink]),
   cache: new InMemoryCache(),
 });
 
