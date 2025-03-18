b9bcb717d3685fd9a42049b9e178ced4baacbcba
Gabriel Alves <gabrielalves6002@gmail.com>
2025-03-18 11:42:20 -0300

    feat: add signs icons

---
+++ src/atomic/icons/signal/index.ts
@@ -0,0 +1,2 @@
+export { default as SignalAdd } from "./add";
+export { default as SignalMore } from "./more";
---
+++ src/atomic/icons/signal/more.tsx
@@ -0,0 +1,18 @@
+const SignalMore = () => {
+  return (
+    <svg
+      className="overflow-visible"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="currentColor"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <circle cx="7" cy="12" r="1.5" />
+      <circle cx="12" cy="12" r="1.5" />
+      <circle cx="17" cy="12" r="1.5" />
+    </svg>
+  );
+};
+
+export default SignalMore;
