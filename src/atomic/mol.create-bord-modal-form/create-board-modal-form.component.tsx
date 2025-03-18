6b100297fb917f094e5f82264a1d28b0e4c9ffe1
Gabriel Alves <gabrielalves6002@gmail.com>
2025-03-18 15:13:11 -0300

    feat: add CreateBoardModalForm

---
+++ src/atomic/mol.create-bord-modal-form/create-board-modal-form.component.tsx
@@ -0,0 +1,53 @@
+import { FC, useState } from "react";
+import Caption from "../atm.caption";
+import { TextFormFields } from "../mol.input-fields";
+import Modal from "../mol.modal";
+import Form from "../org.form";
+import * as createBoardModalFormStrings from "./create-board-modal-form.strings";
+import { useCreateBoard } from "@/app/domain/board/create-board.use-case";
+import { createBoardSchema, CreateBoardData } from "../org.form/form.schemas";
+
+interface ModalProps {
+  isOpen: boolean;
+  onClose: () => void;
+}
+
+const CreateBoardModalForm: FC<ModalProps> = ({ isOpen, onClose }) => {
+  const [errorMsg, setErrorMsg] = useState("");
+
+  const { createBoard, loading } = useCreateBoard({
+    onCompleted() {
+      window.location.reload();
+    },
+    onError(error) {
+      setErrorMsg(error.message);
+    },
+  });
+
+  const handleSubmit = (data: CreateBoardData) => {
+    createBoard({
+      data: { name: data.name },
+    });
+  };
+
+  return (
+    <Modal isOpen={isOpen} onClose={onClose}>
+      <Form
+        onSubmit={handleSubmit}
+        schema={createBoardSchema}
+        buttonDisabled={loading}
+        buttonLabel={
+          loading
+            ? createBoardModalFormStrings.BUTTON_LABEL_CREATE_BOARD_LOADING
+            : createBoardModalFormStrings.BUTTON_LABEL_CREATE_BOARD
+        }
+      >
+        <TextFormFields fields={createBoardModalFormStrings.TEXT_FIELDS} />
+
+        {errorMsg ? <Caption status="error">{errorMsg}</Caption> : undefined}
+      </Form>
+    </Modal>
+  );
+};
+
+export default CreateBoardModalForm;
---
+++ src/atomic/mol.create-bord-modal-form/create-board-modal-form.strings.ts
@@ -0,0 +1,11 @@
+export const TEXT_FIELDS = [
+  {
+    name: "name",
+    label: "Nome",
+    type: "text",
+    placeholder: "Nome do projeto",
+  },
+];
+
+export const BUTTON_LABEL_CREATE_BOARD = "Criar Projeto";
+export const BUTTON_LABEL_CREATE_BOARD_LOADING = "Carregando...";
