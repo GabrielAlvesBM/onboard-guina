89544c6237f4be632e098bf9e51cc0b068764c71
Gabriel Alves <gabrielalves6002@gmail.com>
2025-03-18 15:14:32 -0300

    refactor: apply CreateBoardModalForm

--- src/atomic/mol.empty-project-message/empty-project-message.component.tsx
+++ src/atomic/mol.empty-project-message/empty-project-message.component.tsx
@@ -1,33 +1,12 @@
 import { useState } from "react";
 import Text from "../atm.typography";
 import Button from "../atm.button";
-import Caption from "../atm.caption";
-import { TextFormFields } from "../mol.input-fields";
-import Modal from "../mol.modal";
-import Form from "../org.form";
+import CreateBoardModalForm from "../mol.create-bord-modal-form";
 import { PlaceholderDefault } from "../icons/placeholder";
 import * as noProjectMessageStrings from "./empty-project-message.strings";
-import { createBoardSchema, CreateBoardData } from "../org.form/form.schemas";
-import { useCreateBoard } from "@/app/domain/board/create-board.use-case";
 
 const EmptyProjectMessage = () => {
   const [isCreateBoardModalOpen, setIsCreateBoardModalOpen] = useState(false);
-  const [errorMsg, setErrorMsg] = useState("");
-
-  const { createBoard, loading } = useCreateBoard({
-    onCompleted() {
-      setIsCreateBoardModalOpen(false);
-    },
-    onError(error) {
-      setErrorMsg(error.message);
-    },
-  });
-
-  const handleSubmit = (data: CreateBoardData) => {
-    createBoard({
-      data: { name: data.name },
-    });
-  };
 
   return (
     <>
@@ -44,25 +23,10 @@ const EmptyProjectMessage = () => {
         </Button>
       </div>
 
-      <Modal
+      <CreateBoardModalForm
         isOpen={isCreateBoardModalOpen}
         onClose={() => setIsCreateBoardModalOpen(false)}
-      >
-        <Form
-          onSubmit={handleSubmit}
-          schema={createBoardSchema}
-          buttonDisabled={loading}
-          buttonLabel={
-            loading
-              ? noProjectMessageStrings.BUTTON_LABEL_CREATE_BOARD_LOADING
-              : noProjectMessageStrings.BUTTON_LABEL_CREATE_BOARD
-          }
-        >
-          <TextFormFields fields={noProjectMessageStrings.TEXT_FIELDS} />
-
-          {errorMsg ? <Caption status="error">{errorMsg}</Caption> : undefined}
-        </Form>
-      </Modal>
+      />
     </>
   );
 };
--- src/atomic/mol.empty-project-message/empty-project-message.strings.ts
+++ src/atomic/mol.empty-project-message/empty-project-message.strings.ts
@@ -2,15 +2,3 @@ export const NO_PROJECT = "Nenhum projeto";
 export const NO_PROJECT_DESC = "Você ainda não tem um projeto criado.";
 
 export const CREATE_PROJECT = "Criar Projeto";
-
-export const TEXT_FIELDS = [
-  {
-    name: "name",
-    label: "Nome",
-    type: "text",
-    placeholder: "Nome do projeto",
-  },
-];
-
-export const BUTTON_LABEL_CREATE_BOARD = "Criar Projeto";
-export const BUTTON_LABEL_CREATE_BOARD_LOADING = "Carregando...";
