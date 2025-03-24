2f134e973c653daaf140d25a72fe8d9b21287226
Gabriel Alves <gabrielalves6002@gmail.com>
2025-03-24 12:00:49 -0300

    feat: delete-board-modal

---
+++ src/atomic/mol.delete-board-modal/delete-board-modal.component.tsx
@@ -0,0 +1,64 @@
+import { FC } from "react";
+import Text from "../atm.typography";
+import Button from "../atm.button";
+import Modal from "../mol.modal";
+import * as deleteBoardModalStrings from "./delete-board-modal.strings";
+import { useDeleteBoard } from "@/app/domain/board/delete-board.use-case";
+import { toast } from "sonner";
+
+interface DeleteBoardModalProps {
+  id: string;
+  refetch: () => void;
+  isOpen: boolean;
+  onClose: () => void;
+}
+
+const DeleteBoardModal: FC<DeleteBoardModalProps> = ({
+  id,
+  isOpen,
+  onClose,
+  refetch,
+}) => {
+  const { deleteBoard, loading } = useDeleteBoard({
+    onCompleted() {
+      toast.success(deleteBoardModalStrings.DELETE_SUCCESS);
+      refetch();
+      onClose();
+    },
+    onError(error) {
+      toast.error(deleteBoardModalStrings.DELETE_ERROR, {
+        description: error.message,
+      });
+    },
+  });
+
+  const handleDeleteBoard = () => {
+    deleteBoard({ boardId: id });
+  };
+
+  return (
+    <Modal isOpen={isOpen} onClose={onClose}>
+      <Text variant="body1" className="mb-sm">
+        {deleteBoardModalStrings.CONFIRM_DESCRIPTION}
+      </Text>
+
+      <div className="flex justify-center gap-xs">
+        <Button variant="secondary" onClick={onClose}>
+          {deleteBoardModalStrings.CANCEL}
+        </Button>
+
+        <Button
+          variant="primaryDestructive"
+          disabled={loading}
+          onClick={handleDeleteBoard}
+        >
+          {loading
+            ? deleteBoardModalStrings.LOADING
+            : deleteBoardModalStrings.DELETE_BUTTON}
+        </Button>
+      </div>
+    </Modal>
+  );
+};
+
+export default DeleteBoardModal;
---
+++ src/atomic/mol.delete-board-modal/delete-board-modal.strings.ts
@@ -0,0 +1,8 @@
+export const CONFIRM_DESCRIPTION = "Deseja realmente deletar este projeto?";
+
+export const CANCEL = "Cancelar";
+export const DELETE_BUTTON = "Deletar";
+export const LOADING = "Carregando...";
+
+export const DELETE_SUCCESS = "Projeto deletado com sucesso!";
+export const DELETE_ERROR = "Erro ao Deletar Projeto.";
