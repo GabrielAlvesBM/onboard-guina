cf48d4f13ddafa789bd57bbd34cadd5b29c7af08
Gabriel Alves <gabrielalves6002@gmail.com>
2025-03-24 14:48:43 -0300

    feat: query-board.use-case

---
+++ src/app/domain/board/query-board.use-case.ts
@@ -0,0 +1,29 @@
+import { useQuery } from "@apollo/client";
+import {
+  QueryBoardDocument,
+  QueryBoardQuery,
+  QueryBoardQueryVariables,
+} from "@/app/data/graphql/generated";
+
+interface UseQueryBoardProps {
+  variables: QueryBoardQueryVariables;
+  onCompleted?: (data?: QueryBoardQuery) => void;
+  onError?: (error: Error) => void;
+}
+
+export const useQueryBoard = ({
+  variables,
+  onCompleted,
+  onError,
+}: UseQueryBoardProps) => {
+  const { data, loading } = useQuery(QueryBoardDocument, {
+    variables: variables,
+    onCompleted,
+    onError,
+  });
+
+  return {
+    boardData: data,
+    loading,
+  };
+};
