8716ee6e675a764f7ff759d55dff679e8eddef11
Gabriel Alves <gabrielalves6002@gmail.com>
2025-03-25 09:22:41 -0300

    refactor: enum in CARD_COLUMNS

--- src/atomic/mol.column/column.strings.ts
+++ src/atomic/mol.column/column.strings.ts
@@ -1,8 +1,8 @@
 export const ADD_TASK = "Adicionar tarefa";
 
-export const CARD_COLUMNS = {
-  TO_DO: "A fazer",
-  IN_PROGRESS: "Fazendo",
-  IN_REVIEW: "Review",
-  DONE: "Finalizado",
-};
+export enum CARD_COLUMNS {
+  TO_DO = "A fazer",
+  IN_PROGRESS = "Fazendo",
+  IN_REVIEW = "Review",
+  DONE = "Finalizado",
+}
